34cf65e5daee1d6e548cb6e0211c77b6a109d3f5
tconnors <>
2019-06-29 12:20:53 +0000

    setbright, settext and countdown/up have been implemented

--- template.h
+++ template.h
@@ -5,4 +5,11 @@ void setup_stub();
 void loop_stub();
 String http_uptime_stub();
 
+void ledBright(unsigned int val);
+void ledRamp(int start, int finish, unsigned int duration, unsigned int steps);
+int getArgValue(String name);
+
+extern int ONBOARD_LED_PIN;
+extern int led_range;
+
 #endif
